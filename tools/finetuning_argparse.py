cd22dfa5cfee612b7c9f16011d7c7f090d855917
Yanghello <915769235@qq.com>
2024-04-21 08:54:24 +0800

    add bert

--- tools/finetuning_argparse.py
+++ tools/finetuning_argparse.py
@@ -105,5 +105,4 @@ def get_argparse():
     parser.add_argument("--local_rank", type=int, default=-1, help="For distributed training: local_rank")
     parser.add_argument("--server_ip", type=str, default="", help="For distant debugging.")
     parser.add_argument("--server_port", type=str, default="", help="For distant debugging.")
-    parser.add_argument("--n_jobs", type=int, default=4, help="number of jobs")
     return parser
